19be547de16e39105ed1f25c9e6656a9f7c2e550
Daniel Olson <emaildano@gmail.com>
2019-11-30 13:05:10 -0500

    Added fonts

---
+++ scripts/fonts.sh
@@ -0,0 +1,20 @@
+# Install System Fonts
+# --------
+
+# Tap Fonts Cask
+# -------------
+brew tap homebrew/cask-fonts
+
+# Fonts
+# Search caskroom using `brew search font-open-sans`
+fonts=(
+  font-inconsolata
+  font-montserrat
+  font-open-sans
+  font-source-code-pro
+  font-source-han-code-jp
+)
+
+# install fonts
+echo "installing fonts..."
+brew cask install ${fonts[@]}
